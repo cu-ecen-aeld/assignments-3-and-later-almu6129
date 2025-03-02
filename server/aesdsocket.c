92bb077fcaefb28d01a2435f1bf6692223a22cac
almu6129 <almu6129@colorado.edu>
2025-03-01 17:37:17 -0700

    Looks as though part 1.1 is satisfied

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -11,24 +11,61 @@
 #include <signal.h>
 #include <pthread.h>
 
+#include "queue.h"
+
 #define PORT_NUM 9000
 #define BUFSIZE 512
 
+struct thread_context_t
+{
+	char ip_add[30];
+	int new_sock;
+	//shared pointer to the lock
+	pthread_mutex_t *file_mutex_lock;
+	pthread_t sniffer_thread;
+
+};
+
+typedef struct thread_context_t thread_context;
+
+struct node
+{
+    thread_context *thread_info;
+    TAILQ_ENTRY(node) nodes;
+};
+
+
+typedef TAILQ_HEAD(head_s, node) head_t;
+
 int stop_flag;
+pthread_mutex_t *list_mutex_lock;
 
 void signal_handler(int signal_num);
 int sendall(int s, char *buf, int *len);
 void *response_handler(void *);
+void *joiner_handler(void *);
 
 int main(int argc, char * argv[]){
 
-	int sockfd, n, clientfd, client_sock, *new_sock;
+	int sockfd, n, clientfd, client_sock;
 	struct sockaddr_in serveraddr; /* server's addr */
   	struct sockaddr_in clientaddr; /* client addr */
   	struct hostent *hostp; /* client host info */
 	char *hostaddrp;
 	struct sigaction sa;
 	stop_flag = 0;
+	pthread_t garbage_collector_thread;
+
+	pthread_mutex_t file_mutex;
+	pthread_mutex_init(&file_mutex, NULL);
+
+	pthread_mutex_t list_mutex;
+	pthread_mutex_init(&list_mutex, NULL);
+
+	list_mutex_lock = &list_mutex;
+
+	head_t head;
+    TAILQ_INIT(&head);
 
 	fflush(stdin);
 
@@ -106,63 +143,49 @@ int main(int argc, char * argv[]){
 		int clientlen = sizeof(clientaddr);
 
 		listen(sockfd , 3);
+
+		pthread_create( &garbage_collector_thread , NULL ,  joiner_handler , (void*) &head);
 		
 		int c = sizeof(struct sockaddr_in);
 
 		while(1 && !stop_flag){
 			
-			while(client_sock = accept(sockfd, (struct sockaddr *)&clientaddr, (socklen_t*)&c)){
+			while((client_sock = accept(sockfd, (struct sockaddr *)&clientaddr, (socklen_t*)&c)) && !stop_flag){
 
 				hostaddrp = inet_ntoa(clientaddr.sin_addr);
 				syslog(LOG_INFO, "Accepted connection from %s", hostaddrp);
 
-				pthread_t sniffer_thread;
-				new_sock = malloc(1);
-				*new_sock = client_sock;
+				thread_context * thread_info = (thread_context *)malloc(sizeof(thread_context));
 
-				if( pthread_create( &sniffer_thread , NULL ,  response_handler , (void*) new_sock) < 0)
-				{
-					perror("could not create thread");
-				}
-				if(pthread_detach(sniffer_thread) != 0){
-					perror("Error detaching thread");
+				if(thread_info == NULL){
+					perror("error with malloc");
 					return -1;
 				}
 
+				thread_info -> file_mutex_lock = &file_mutex;
+				thread_info -> new_sock = client_sock;
+				strncpy(thread_info -> ip_add, hostaddrp, strlen(hostaddrp));
+
+				if( pthread_create( &thread_info -> sniffer_thread , NULL ,  response_handler , (void*) thread_info) < 0)
+				{
+					perror("could not create thread");
+					return -1;
+				}
+				
+				struct node * e = malloc(sizeof(struct node));
+
+				if(e == NULL){
+					perror("error with malloc");
+					return -1;
+				}
+
+				e -> thread_info = thread_info;
+
+				pthread_mutex_lock(list_mutex_lock);
+				TAILQ_INSERT_TAIL(&head, e, nodes);
+				pthread_mutex_unlock(list_mutex_lock);
+
 			}
-
-			
-
-			//need to change this
-
-			void * to_echo_back = malloc(file_size);
-
-			if(to_echo_back == NULL){
-				perror("malloc error");
-				closelog();
-				return -1;
-			}
-
-			int ret_val_read = read(fd, to_echo_back, file_size);
-			
-			if(ret_val_read == -1){
-				perror("error with read");
-				closelog();
-				free(to_echo_back);
-				return -1;
-			}
-
-			n = sendall(clientfd, to_echo_back, (int *)&file_size);
-        	if (n < 0){
-          		perror("ERROR in sendto");
-				free(to_echo_back);
-				closelog();
-				return -1;
-			}
-
-			syslog(LOG_INFO, "Closed connection from %s", hostaddrp);
-			free(to_echo_back);
-
 		}
 
 	}
@@ -171,38 +194,37 @@ int main(int argc, char * argv[]){
 		closelog();
 		return -1;
 	}
+
+	pthread_join(garbage_collector_thread, NULL);
+
 	closelog();
 	return 0;
 
 }
 
-void *response_handler(void *socket_desc){
+void *response_handler(void *thread_info){
 
 	char buf[BUFSIZE];
-	int sock = *(int*)socket_desc;
+	thread_context * total_context = (thread_context *)thread_info;
 	int n;
 	char *token;
 	int found_terminator;
+	int bytes_read;
 
 	int fd = open("/var/tmp/aesdsocketdata", O_CREAT|O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 
 	if(fd == -1){
 		perror("error with open");
-		closelog();
-		close(sock);
-		return NULL;
+		return thread_info;
 	}
 
 	while(1){
 		bzero(buf, BUFSIZE);
-		n = recv(sock, buf, BUFSIZE, 0);
+		n = recv(total_context -> new_sock, buf, BUFSIZE, 0);
 		if (n < 0){
 			perror("ERROR in recv");
-			closelog();
-			close(sock);
-			return NULL;
+			return thread_info;
 		}
-		printf("read: %d bytes\n", n);
 		
 		found_terminator = 0;
 
@@ -214,10 +236,10 @@ void *response_handler(void *socket_desc){
 
 		if((token = strtok(buf, "\n")) == NULL){
 			fprintf(stderr, "Ran into issues with finding a full packet\n");
-			closelog();
-			close(sock);
-			return NULL;
+			return thread_info;
 		}
+
+		pthread_mutex_lock(total_context -> file_mutex_lock);
 
 		lseek(fd, 0, SEEK_END);
 
@@ -225,22 +247,66 @@ void *response_handler(void *socket_desc){
 
 		if(write(fd, token, len_to_write) != len_to_write){
 			fprintf(stderr, "Ran into issues with sending a full packet\n");
-			closelog();
-			return -1;
+			pthread_mutex_unlock(total_context -> file_mutex_lock);
+			return thread_info;
 		}
+		pthread_mutex_unlock(total_context -> file_mutex_lock);
 
 		if(found_terminator) break;
 
 	}
 
+	pthread_mutex_lock(total_context -> file_mutex_lock);
+
 	write(fd, "\n", 1);
 
 	lseek(fd, 0, SEEK_SET);
 
-	struct stat file_stat;
-	fstat(fd, &file_stat);
-	off_t file_size = file_stat.st_size;
+	while ((bytes_read = read(fd, buf, BUFSIZE)) > 0) {
+		sendall(total_context -> new_sock, buf, &bytes_read);
+    }
 
+	pthread_mutex_unlock(total_context -> file_mutex_lock);
+
+	if (bytes_read == -1){
+        perror("Error reading file");
+        close(fd);
+        return thread_info;
+    }
+
+	close(fd);
+	return thread_info;
+}
+
+void *joiner_handler(void *arr){
+
+	head_t * head = (head_t *)arr;
+	struct node * e = NULL;
+	struct node * next = NULL;
+
+	while(!stop_flag){
+
+		pthread_mutex_lock(list_mutex_lock);
+
+		if(!TAILQ_EMPTY(head)){
+
+			TAILQ_FOREACH_SAFE(e, head, nodes, next)
+			{
+				if(pthread_tryjoin_np(e->thread_info->sniffer_thread, NULL) == 0){
+
+					syslog(LOG_INFO, "Closed connection from %s", e->thread_info->ip_add);
+					TAILQ_REMOVE(head, e, nodes);
+					free(e -> thread_info);
+					free(e);
+					e = NULL;
+				}
+			}
+		}
+
+		pthread_mutex_unlock(list_mutex_lock);
+		//periodically examine the list
+		usleep(200000);
+	}
 }
 
 void signal_handler(int signal_num) {
